9a44b2fe5a42ae6dc68f69281a04d0689a38c8d2
Emil Stenqvist <emsten@gmail.com>
2014-08-13 22:31:37 +0200

    Add extracted content to EverNote

--- everclip.py
+++ everclip.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 import re
+
 from imbox import Imbox
 from goose import Goose
+from evernote.api.client import EvernoteClient
+import evernote.edam.type.ttypes as Types
+
 import settings
 
 from emilisto.interact import interact
@@ -21,6 +25,10 @@ class Everclip(object):
         self.imbox = Imbox(settings.IMAP_HOST, username=settings.IMAP_USERNAME,
                 password=settings.IMAP_PASSWORD)
 
+        self.evernote = EvernoteClient(token=settings.EVERNOTE_TOKEN,
+                sandbox=False)
+        self.noteStore = self.evernote.get_note_store()
+
     def get_new_emails(self):
         emails = self.imbox.messages(sent_to=settings.TO_ADDRESS, unread=True)
         return list(emails)
@@ -28,15 +36,28 @@ class Everclip(object):
     def process_email(self, email):
         # Plain is given as a list - in case of multipart perhaps?
 
+        # 1. Identify URL's in email
         plain = ' '.join(email.body['plain'])
         urls = extract_urls_from_text(plain)
         # NOTE: what to do about multiple links? include all of them? For now,
         # just choose the first one.
         url = urls[0]
 
-        title, content = extract_url_content(url)
-        print content
-        # TODO: add to EverNote
+        # 2. Extract URL content
+        title, text = extract_url_content(url)
+
+        # 3. Add to EverNote
+        note = Types.Note()
+        note.title = title
+        content = u'<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
+        content += u'<en-note>{}</en-note>'.format(text)
+        note.content = content.encode('utf8')
+
+        attributes = Types.NoteAttributes()
+        attributes.sourceURL = url
+        note.attributes = attributes
+
+        note = self.noteStore.createNote(note)
 
     def run(self):
         print "Checking for new emails..."
