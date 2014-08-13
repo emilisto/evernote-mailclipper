fb23bf00ed538104816a0af75268d4299f1eaca1
Emil Stenqvist <emsten@gmail.com>
2014-08-13 21:49:10 +0200

    Working

--- everclip.py
+++ everclip.py
@@ -14,16 +14,22 @@ class Everclip(object):
                 password=settings.IMAP_PASSWORD)
 
     def get_new_emails(self):
-        return self.imbox.messages(sent_to=settings.TO_ADDRESS, unread=True)
+        emails = self.imbox.messages(sent_to=settings.TO_ADDRESS, unread=True)
+        return list(emails)
 
     def run(self):
-        everclip = Everclip()
         print "Checking for new emails..."
-        emails = list(everclip.get_new_emails())
+        emails = self.get_new_emails()
         print "No. of new emails: %d" % (len(emails),)
         for id, email in emails:
-            interact()
+            # Plain is given as a list - in case of multipart perhaps?
+            plain = ' '.join(email.body['plain'])
+
+            links = extract_links_from_text(plain)
+            print links
+            # TODO: scrape the supplied article using Goose
             # TODO: mark as read
+            # TODO: add to EverNote
 
 
 def extract_links_from_text(text):
